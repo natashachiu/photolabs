9f76e50bd2107da155eedd1c3b79e3daa9293642
Natasha Chiu <chiuchiu07@gmail.com>
2023-08-15 06:47:25 +0800

    cleanup

--- frontend/src/hooks/useSearchBar.js
+++ frontend/src/hooks/useSearchBar.js
@@ -2,7 +2,6 @@ import { useState } from 'react';
 
 const useSearchBar = (onSubmit) => {
   const [searchTerm, setSearchTerm] = useState("");
-  // console.log(searchTerm);
 
   const handleSubmit = (e) => {
     e.preventDefault();
